9f407d06b0586785cdb62db2849d62ff3ba2d5f3
vdarth567 <67248106+vdarth567@users.noreply.github.com>
2024-09-04 03:17:47 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,45 +4,45 @@ from bot import CMD_SUFFIX, config_dict
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = [f'mirror{CMD_SUFFIX}', f'm{CMD_SUFFIX}']
-        self.QbMirrorCommand = [f'qbmirror{CMD_SUFFIX}', f'qm{CMD_SUFFIX}']
-        self.YtdlCommand = [f'ytdl{CMD_SUFFIX}', f'y{CMD_SUFFIX}']
-        self.LeechCommand = [f'leech{CMD_SUFFIX}', f'l{CMD_SUFFIX}']
-        self.QbLeechCommand = [f'qbleech{CMD_SUFFIX}', f'ql{CMD_SUFFIX}']
-        self.YtdlLeechCommand = [f'ytdlleech{CMD_SUFFIX}', f'yl{CMD_SUFFIX}']
+        self.MirrorCommand = [f'pmirror{CMD_SUFFIX}', f'pm{CMD_SUFFIX}']
+        self.QbMirrorCommand = [f'pqbmirror{CMD_SUFFIX}', f'pqm{CMD_SUFFIX}']
+        self.YtdlCommand = [f'pytdl{CMD_SUFFIX}', f'py{CMD_SUFFIX}']
+        self.LeechCommand = [f'pleech{CMD_SUFFIX}', f'pl{CMD_SUFFIX}']
+        self.QbLeechCommand = [f'pqbleech{CMD_SUFFIX}', f'pql{CMD_SUFFIX}']
+        self.YtdlLeechCommand = [f'pytdlleech{CMD_SUFFIX}', f'pyl{CMD_SUFFIX}']
         if config_dict['SHOW_EXTRA_CMDS']:
-            self.MirrorCommand.extend([f'unzipmirror{CMD_SUFFIX}', f'uzm{CMD_SUFFIX}', f'zipmirror{CMD_SUFFIX}', f'zm{CMD_SUFFIX}'])
-            self.QbMirrorCommand.extend([f'qbunzipmirror{CMD_SUFFIX}', f'quzm{CMD_SUFFIX}', f'qbzipmirror{CMD_SUFFIX}', f'qzm{CMD_SUFFIX}'])
-            self.YtdlCommand.extend([f'ytdlzip{CMD_SUFFIX}', f'yz{CMD_SUFFIX}'])
-            self.LeechCommand.extend([f'unzipleech{CMD_SUFFIX}', f'uzl{CMD_SUFFIX}', f'zipleech{CMD_SUFFIX}', f'zl{CMD_SUFFIX}'])
-            self.QbLeechCommand.extend([f'qbunzipleech{CMD_SUFFIX}', f'quzl{CMD_SUFFIX}', f'qbzipleech{CMD_SUFFIX}', f'qzl{CMD_SUFFIX}'])
-            self.YtdlLeechCommand.extend([f'ytdlzipleech{CMD_SUFFIX}', f'yzl{CMD_SUFFIX}'])
-        self.CloneCommand = [f'clone{CMD_SUFFIX}', f'c{CMD_SUFFIX}']
-        self.CountCommand = f'count{CMD_SUFFIX}'
-        self.DeleteCommand = f'del{CMD_SUFFIX}'
-        self.CancelMirror = f'cancel{CMD_SUFFIX}'
-        self.CancelAllCommand = [f'cancelall{CMD_SUFFIX}', 'cancellallbot']
-        self.ListCommand = f'list{CMD_SUFFIX}'
-        self.SearchCommand = f'search{CMD_SUFFIX}'
-        self.StatusCommand = [f'status{CMD_SUFFIX}', f's{CMD_SUFFIX}', 'statusall']
-        self.UsersCommand = f'users{CMD_SUFFIX}'
-        self.AuthorizeCommand = [f'authorize{CMD_SUFFIX}', f'a{CMD_SUFFIX}']
-        self.UnAuthorizeCommand = [f'unauthorize{CMD_SUFFIX}', f'ua{CMD_SUFFIX}']
-        self.AddBlackListCommand = [f'blacklist{CMD_SUFFIX}', f'bl{CMD_SUFFIX}']
-        self.RmBlackListCommand = [f'rmblacklist{CMD_SUFFIX}', f'rbl{CMD_SUFFIX}']
+            self.MirrorCommand.extend([f'punzipmirror{CMD_SUFFIX}', f'puzm{CMD_SUFFIX}', f'pzipmirror{CMD_SUFFIX}', f'pzm{CMD_SUFFIX}'])
+            self.QbMirrorCommand.extend([f'pqbunzipmirror{CMD_SUFFIX}', f'pquzm{CMD_SUFFIX}', f'pqbzipmirror{CMD_SUFFIX}', f'pqzm{CMD_SUFFIX}'])
+            self.YtdlCommand.extend([f'pytdlzip{CMD_SUFFIX}', f'pyz{CMD_SUFFIX}'])
+            self.LeechCommand.extend([f'punzipleech{CMD_SUFFIX}', f'puzl{CMD_SUFFIX}', f'pzipleech{CMD_SUFFIX}', f'zl{CMD_SUFFIX}'])
+            self.QbLeechCommand.extend([f'pqbunzipleech{CMD_SUFFIX}', f'pquzl{CMD_SUFFIX}', f'pqbzipleech{CMD_SUFFIX}', f'pqzl{CMD_SUFFIX}'])
+            self.YtdlLeechCommand.extend([f'pytdlzipleech{CMD_SUFFIX}', f'pyzl{CMD_SUFFIX}'])
+        self.CloneCommand = [f'pclone{CMD_SUFFIX}', f'c{CMD_SUFFIX}']
+        self.CountCommand = f'pcount{CMD_SUFFIX}'
+        self.DeleteCommand = f'pdel{CMD_SUFFIX}'
+        self.CancelMirror = f'pcancel{CMD_SUFFIX}'
+        self.CancelAllCommand = [f'pcancelall{CMD_SUFFIX}', 'cancellallbot']
+        self.ListCommand = f'plist{CMD_SUFFIX}'
+        self.SearchCommand = f'psearch{CMD_SUFFIX}'
+        self.StatusCommand = [f'pstatus{CMD_SUFFIX}', f's{CMD_SUFFIX}', 'statusall']
+        self.UsersCommand = f'pusers{CMD_SUFFIX}'
+        self.AuthorizeCommand = [f'pauthorize{CMD_SUFFIX}', f'a{CMD_SUFFIX}']
+        self.UnAuthorizeCommand = [f'punauthorize{CMD_SUFFIX}', f'ua{CMD_SUFFIX}']
+        self.AddBlackListCommand = [f'pblacklist{CMD_SUFFIX}', f'bl{CMD_SUFFIX}']
+        self.RmBlackListCommand = [f'prmblacklist{CMD_SUFFIX}', f'rbl{CMD_SUFFIX}']
         self.AddSudoCommand = f'addsudo{CMD_SUFFIX}'
         self.RmSudoCommand = f'rmsudo{CMD_SUFFIX}'
-        self.PingCommand = [f'ping{CMD_SUFFIX}', f'p{CMD_SUFFIX}']
-        self.RestartCommand = [f'restart{CMD_SUFFIX}', f'r{CMD_SUFFIX}', 'restartall']
+        self.PingCommand = [f'pping{CMD_SUFFIX}', f'p{CMD_SUFFIX}']
+        self.RestartCommand = [f'prestart{CMD_SUFFIX}', f'r{CMD_SUFFIX}', 'restartall']
         self.StatsCommand = [f'stats{CMD_SUFFIX}', f'st{CMD_SUFFIX}']
-        self.HelpCommand = f'help{CMD_SUFFIX}'
-        self.LogCommand = f'log{CMD_SUFFIX}'
+        self.HelpCommand = f'phelp{CMD_SUFFIX}'
+        self.LogCommand = f'plog{CMD_SUFFIX}'
         self.ShellCommand = f'shell{CMD_SUFFIX}'
         self.EvalCommand = f'eval{CMD_SUFFIX}'
         self.ExecCommand = f'exec{CMD_SUFFIX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_SUFFIX}'
-        self.BotSetCommand = [f'bsetting{CMD_SUFFIX}', f'bs{CMD_SUFFIX}']
-        self.UserSetCommand = [f'usetting{CMD_SUFFIX}', f'us{CMD_SUFFIX}']
+        self.BotSetCommand = [f'pbsetting{CMD_SUFFIX}', f'bs{CMD_SUFFIX}']
+        self.UserSetCommand = [f'pusetting{CMD_SUFFIX}', f'us{CMD_SUFFIX}']
         self.BtSelectCommand = f'btsel{CMD_SUFFIX}'
         self.CategorySelect = f'ctsel{CMD_SUFFIX}'
         self.SpeedCommand = [f'speedtest{CMD_SUFFIX}', f'sp{CMD_SUFFIX}']
