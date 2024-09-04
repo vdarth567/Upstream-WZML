4ee4e1c4eb72e877387561e3e444953384510be1
vdarth567 <67248106+vdarth567@users.noreply.github.com>
2024-09-04 18:00:30 +0530

    Update init.py

--- bot/init.py
+++ bot/init.py
@@ -1,4 +1,4 @@
 from pyrogram import utils as pyroutils
 
-pyroutils.MIN_CHAT_ID = -999999999999
-pyroutils.MIN_CHANNEL_ID = -100999999999999
+pyroutils.MIN_CHAT_ID = -2177210329
+pyroutils.MIN_CHANNEL_ID = -1002177210329
